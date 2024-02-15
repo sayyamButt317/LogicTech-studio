b270411313319f4255b2132dc78f33735763ce67
IT <sayyambutt317@gmail.com>
2024-02-16 02:24:03 +0500

    server

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,17 @@
-const express = require('express')
+const express = require("express");
 const mongoose = require("mongoose");
-const app = express()
+const app = express();
 const port = process.env.PORT;
-const cookiePaser = require('cookie-parser');
-const {connectToMongoDb} = require("./connection");
+const cookiePaser = require("cookie-parser");
+const { connectToMongoDb } = require("./connection");
 const path = require("path");
 const USER = require("./model/formSchema");
 const RouteHandler = require("./Routes/staticroute");
 
-connectToMongoDb('mongodb://0.0.0.0:27017/LogiTech')
-  .then(() => console.log("Mongo DB connected "));
+connectToMongoDb("mongodb://0.0.0.0:27017/LogiTech").then(() =>
+  console.log("Mongo DB connected ")
+);
 
-  
 // const DB = "mongodb+srv://sayyambutt317:fallrBw4ZaW@Logictech.mongodb.net/?retryWrites=true&w=majority";
 //   mongoose.connect(DB,{
 //     useNewUrlParser:true,
@@ -21,13 +21,11 @@ connectToMongoDb('mongodb://0.0.0.0:27017/LogiTech')
 //   }).catch((err)=> {
 //     console.log(`no conection ${err}`)
 //   })
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookiePaser());
-app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-app.use("/",RouteHandler);
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.use("/", RouteHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
