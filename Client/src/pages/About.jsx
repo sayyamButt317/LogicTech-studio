994df6cb24a5f5bd21dd30a34eafa9cb9101d1dd
IT <sayyambutt317@gmail.com>
2024-02-24 03:46:04 +0500

    ui

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import AboutHero from '../components/About/AboutHero'
 import Aboutus from '../components/About/Aboutus'
 import Mission from '../components/About/mission'
+import Advancement from '../components/About/Advancement'
 function About() {
   return (
 <>
     <AboutHero/>
     <Aboutus/>
     <Mission/>
+    <Advancement/>
 </>
   )
 }
