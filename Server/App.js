7284555e3a45a81b396a2f7aade3ed2819bfbfed
sayyambutt <sayyambutt317@gmail.com>
2024-07-10 16:29:41 +0500

    Set Middleware and default Route

--- Server/App.js
+++ Server/App.js
@@ -2,24 +2,22 @@ import Express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRouter from "./Routes/static.routes.js";
+
 const app = Express();
 
-//Middleware
+// Middleware
 app.use(
     cors({
-      origin: process.env.CORS_ORIGN,
-      credentials: true,
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
-  );
-
+);
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
 
+// Routes Declaration
+app.use("/api/v1/", userRouter);
 
-//Routes Decleration
-// app.use("/api/v1/save-form", userRouter);
-app.use("/api/v1/", () => userRouter);
 export { app };
