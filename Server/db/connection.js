c907375d1216b0adf31fa532e7c72b5c50c07a54
sayyambutt <sayyambutt317@gmail.com>
2024-07-10 16:30:44 +0500

    Create db connection using by using Environments variables

--- Server/db/connection.js
+++ Server/db/connection.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../Utils/constant.js";
 
 const connectionDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(
-            `${process.env.MONGODB_URI}/${DB_NAME}`
+            `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
         );
         console.log(
             `\n Connected to MongoDB! ${connectionInstance.connection.host}`
         );
     } catch (error) {
-        console.log("MongoDB Connection Failed", error);
+        console.log("MongoDB connection failed", error);
         process.exit(1);
     }
 };
